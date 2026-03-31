21ab5c0aa6cacdcaf0e3c756700f5a26e2a0187d
Gaurav Kumar <princegauravaj@gmail.com>
2026-03-31 16:17:57 +0530

    test

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ initCronJobs();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    // reCAPTCHA keys updated, forcing reload
+  console.log(`Server is running on port ${PORT}`);
+  // reCAPTCHA keys updated, forcing reload
 });
